8157f1f319355d508d7a80489572274e1fc2e31f
Herbert118 <72267704+Herbert118@users.noreply.github.com>
2023-11-08 11:24:53 +0800

    test2

--- app/test/customers/dist/page.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function Page() {
-    return React.createElement("p", null, "customers test");
-}
-exports["default"] = Page;
--- app/ui/dist/font.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.inter = void 0;
-var google_1 = require("next/font/google");
-exports.inter = google_1.Inter({ subsets: ['latin'] });
